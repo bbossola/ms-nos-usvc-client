7b14f94d5642c2837d9d9617fb1996e1fe9cdc55
Bruno Bossola <bbossola@gmail.com>
2015-03-06 18:26:54 +0000

    Delete deploy-cloud.sh

--- deploy-cloud.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-scp -i /home/bossola/.ssh/singapore_rsa target/ms-nos-usvc-client-0.0.1-SNAPSHOT.jar ubuntu@54.66.198.2:/home/ubuntu/
-scp -i /home/bossola/.ssh/singapore_rsa target/ms-nos-usvc-client-0.0.1-SNAPSHOT.jar ubuntu@54.169.180.250:/home/ubuntu/
-scp -i /home/bossola/.ssh/singapore_rsa target/ms-nos-usvc-client-0.0.1-SNAPSHOT.jar ubuntu@bb-chinatest.cloudapp.net:/home/ubuntu/
-scp target/ms-nos-usvc-client-0.0.1-SNAPSHOT.jar bbossola@microservices.dev2.workshare.com:/home/bbossola/
-
